3a0ed75a5f5d061da0216a78651bc8b0e95d4c5e
Kaiqueav <kaiquecerq66@outlook.com>
2024-02-07 00:00:42 -0300

    fix

--- src/components/cardNew/CardNew.tsx
+++ src/components/cardNew/CardNew.tsx
@@ -1,6 +1,6 @@
 export const CardNew = () =>{
     return(
-        <div className="rounded-md bg-slate-700 p-5 space-y-3">
+        <div className="rounded-md bg-slate-700 p-5 space-y-3 hover:ring-2">
             <span className="text-sm font-medium text-slate-200"> Adicionar Nota</span>
             <p className="text-sm leading-6 text-slate-600"> Grave uma nota em audio que sera convertida em texto</p>
         </div>
