7f7765c88aa7f11b02df15a4c426866c7a68c44f
Kaiqueav <kaiquecerq66@outlook.com>
2024-02-06 23:21:10 -0300

    feat: create search bar

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,19 @@ import Logo from './assets/Logo.svg';
 function App() {
 
   return (
-    <>
-     <img src={Logo}  /> 
-     
-    </>
+    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+      <img src={Logo} />
+
+      <form action="" className='w-full'>
+        <input type="text"
+          name=""
+          placeholder="Busque suas notas" 
+          className='w-full bg-transparent text-3xl font-semibold tracking-tighter outline-none placeholder:text-slate-500'
+          />
+      </form>
+
+
+    </div>
   )
 }
 
