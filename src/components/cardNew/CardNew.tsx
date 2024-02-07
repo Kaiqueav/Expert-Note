a73cd63b2d841bc2e46832129eb1e1ede68e6415
Kaiqueav <kaiquecerq66@outlook.com>
2024-02-06 23:41:55 -0300

    feat: criando card para inserção de nota

---
+++ src/components/cardNew/CardNew.tsx
@@ -0,0 +1,8 @@
+export const CardNew = () =>{
+    return(
+        <div className="rounded-md bg-slate-700 p-5 space-y-3">
+            <span className="text-sm font-medium text-slate-200"> Adicionar Nota</span>
+            <p className="text-sm leading-6 text-slate-600"> Grave uma nota em audio que sera convertida em texto</p>
+        </div>
+    )
+}
