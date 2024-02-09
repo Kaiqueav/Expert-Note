0045583094f6ed0fc080fb1c35f58f85838c59d2
Kaiqueav <kaiquecerq66@outlook.com>
2024-02-08 22:26:01 -0300

    feat:  criando função para inciar gravação

--- src/components/cardNew/CardNew.tsx
+++ src/components/cardNew/CardNew.tsx
@@ -1,40 +1,43 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { X } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
-import { toast} from 'sonner'
+import { toast } from 'sonner'
 
 interface CardNewProps {
-    onNoteCreated : (content: string) => void
+    onNoteCreated: (content: string) => void
 }
 
-export const CardNew = ({ onNoteCreated }:CardNewProps) => {
+export const CardNew = ({ onNoteCreated }: CardNewProps) => {
 
     const [shouldShowOnBoarding, setShouldShowOnBoarding] = useState(true);
     const [content, setContent] = useState('');
-
-    const handleStartEdition = () =>{
+    const [recording, setRecording] = useState(false);
+    const handleStartEdition = () => {
         setShouldShowOnBoarding(false);
     }
 
-
     function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>) {
-        
+
         setContent(event.target.value);
 
-        if(event.target.value===''){
+        if (event.target.value === '') {
             setShouldShowOnBoarding(true);
         }
     }
 
 
-    const handleSaveNote = (event: FormEvent) =>{
+    const handleSaveNote = (event: FormEvent) => {
         event.preventDefault();
-    
+
         onNoteCreated(content)
         setContent('')
         setShouldShowOnBoarding(true)
         toast.success("Nota Criada com sucesso!")
 
+    }
+
+    const handleStartRecording = () => {
+        setRecording(true);
     }
 
 
@@ -51,41 +54,36 @@ export const CardNew = ({ onNoteCreated }:CardNewProps) => {
                 <Dialog.Overlay className="inset-0 fixed bg-black/50" />
                 <Dialog.Content className="fixed rounded-md overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full bg-slate-700 outline-none flex flex-col">
                     <form onSubmit={handleSaveNote} className="flex-1 flex flex-col">
-                        
-                    
-                    <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
-                        <X className="size-5" />
-                    </Dialog.Close>
-                    <div className="flex flex-1 flex-col gap-3 p-5 ">
-                        <span className="text-sm font-medium text-slate-300">  </span>
-
-
-                        {shouldShowOnBoarding ?(
-                            <p className="text-sm leading-6 text-slate-400">
-                            Comece <button className="font-medium text-lime-400 hover:underline">gravando uma nota  </button> em audio <button className="font-medium text-lime-400 hover:underline"   onClick={handleStartEdition} >ou se preferir ultilize apenas texto</button> 
-                            </p>
-                         ) : (
-                            <textarea
-                             className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none" 
-                             onChange={handleContentChanged}
-                             autoFocus
-                             value={content}/>
-                         )}
-                        
-                    </div>
-
-                    <button type="submit" className="w-full font-medium bg-lime-400 py-4 text-center text-sm outline-none text-lime-950 hover:bg-lime-500"
-
-                    >
-                        Salvar Nota?
-                    </button>
+                        <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+                            <X className="size-5" />
+                        </Dialog.Close>
+                        <div className="flex flex-1 flex-col gap-3 p-5 ">
+                            <span className="text-sm font-medium text-slate-300">  </span>
+                            {shouldShowOnBoarding ? (
+                                <p className="text-sm leading-6 text-slate-400">
+                                    Comece <button className="font-medium text-lime-400 hover:underline" type="button" onClick={handleStartRecording}> gravando uma nota  </button> em audio <button className="font-medium text-lime-400 hover:underline" type="button" onClick={handleStartEdition} >ou se preferir ultilize apenas texto</button>
+                                </p>
+                            ) : (
+                                <textarea
+                                    className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
+                                    onChange={handleContentChanged}
+                                    autoFocus
+                                    value={content} />
+                            )}
+                        </div>
+                        {recording ? (<button type="submit" className="w-full font-medium bg-slate-900 py-4 text-center text-sm outline-none text-white hover:bg-lime-500"
+                        >
+                            Gravando! ( clique para interromper)
+                        </button>
+                        ) : (
+                            <button type="submit" className="w-full font-medium bg-lime-400 py-4 text-center text-sm outline-none text-lime-950 hover:bg-lime-500"
+                            >
+                                Salvar Nota?
+                            </button>
+                        )}
                     </form>
                 </Dialog.Content>
-
             </Dialog.Portal>
-
-
         </Dialog.Root>
-
     )
 }
